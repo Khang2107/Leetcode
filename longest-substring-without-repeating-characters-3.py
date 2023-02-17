a4b0eeaa0ac69cbb377f6dcf2ab9cd1558e469db
Khang2107 <vankhang.2107@gmail.com>
2023-02-17 11:38:07 +1030

    add another solution using dictionary

--- longest-substring-without-repeating-characters-3.py
+++ longest-substring-without-repeating-characters-3.py
@@ -27,3 +27,25 @@ def lengthOfLongestSubstring(self, s: str) -> int:
         seen.add(s[fast])
         res = max(res, fast - slow + 1)
     return res
+
+
+# Sol 3 - dictionary
+def lengthOfLongestSubstring(self, s: str) -> int:
+    res = 0
+    slow = 0
+    seen = {}
+    
+    for fast in range(len(s)):
+        # if s[fast] not in seen, keep increasing window size & move right
+        if s[fast] not in seen:
+            res = max(res, fast - slow + 1)
+        # if s[fast] in seen, we have two cases:
+        else:
+            # case 1: s[fast] is inside the current window, change window by moving slow to seen[s[fast]] + 1
+            # case 2: s[fast] is not inside the current window, increase window
+            if seen[s[fast]] < slow:
+                res = max(res, fast - slow + 1)
+            else:
+                slow = seen[s[fast]] + 1
+        seen[s[fast]] = fast
+    return res
