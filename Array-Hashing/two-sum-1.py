47f11e92dedc6b08fecbfe956fb22317b47fdf10
Khang2107 <vankhang.2107@gmail.com>
2023-02-23 16:23:39 +1030

    add sol 2 for problem two-sum

--- Array-Hashing/two-sum-1.py
+++ Array-Hashing/two-sum-1.py
@@ -1,4 +1,6 @@
 def twoSum(self, nums: List[int], target: int) -> List[int]:
+    '''
+    # Sol 1:
     seen = {} # val: index
 
     for right, left in enumerate(nums): # get the index of dict
@@ -7,3 +9,16 @@ def twoSum(self, nums: List[int], target: int) -> List[int]:
             return [seen[remain], right] # return value of right
         seen[left] = right
     return
+    '''
+
+    '''
+    # Sol 2:
+    ans = []
+    
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] == target:
+                ans.append(i)
+                ans.append(j)
+    return ans
+    '''
