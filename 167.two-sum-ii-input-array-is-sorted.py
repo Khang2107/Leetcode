e73b4ea48ef0743cfb6354201e0acaddaf579418
Khang2107 <vankhang.2107@gmail.com>
2023-02-23 19:29:21 +1030

    Implement solution 2 problem 167 Two-sum-ii

--- 167.two-sum-ii-input-array-is-sorted.py
+++ 167.two-sum-ii-input-array-is-sorted.py
@@ -7,17 +7,18 @@
 # @lc code=start
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        ans = []
         left = 0
         right = len(numbers) - 1
         
-        for i in range(len(numbers)):
-            for j in range(i+1, len(numbers)):
-                if numbers[i] + numbers[j] == target:
-                    ans.append(i+1)
-                    ans.append(j+1)
-                # if numbers[i] + numbers[j] <
-        return ans
+        while numbers[left] + numbers[right] != target:
+            total = numbers[left] + numbers[right]
+            
+            if total < target:
+                left += 1
+            else:
+                right -= 1
+        
+        return [left+1, right+1]
         
 # @lc code=end
 
