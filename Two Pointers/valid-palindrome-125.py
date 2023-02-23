d1cef1f002de2eb117e264d1b765307787ee5350
Khang2107 <vankhang.2107@gmail.com>
2023-02-23 18:57:13 +1030

    implement solution 2 problem 125 valid-palindrome

--- Two Pointers/valid-palindrome-125.py
+++ Two Pointers/valid-palindrome-125.py
@@ -37,3 +37,21 @@ def isPalindrome(self, s: str) -> bool:
         left += 1
         right -= 1
     return True
+
+
+def isPalindrome(self, s: str) -> bool:
+    left = 0
+    right = len(s) - 1
+    
+    while left < right:
+        if not s[left].isalnum():
+            left += 1
+        elif not s[right].isalnum():
+            right -= 1
+        else:
+            if s[left].lower() != s[right].lower():
+                return False
+            else:
+                left += 1
+                right -= 1
+    return True
