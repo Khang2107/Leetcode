76146c3e776266339a1b0e24659b4e8d7adafc58
Khang2107 <nguvy056@mymail.unisa.edu.au>
2023-03-17 15:44:13 +1030

    add gitignore

---
+++ 155.min-stack.py
@@ -0,0 +1,46 @@
+#
+# @lc app=leetcode id=155 lang=python
+#
+# [155] Min Stack
+#
+
+# @lc code=start
+class MinStack(object):
+
+    def __init__(self):
+        stack = []
+        minStack = []
+
+    def push(self, val):
+        """
+        :type val: int
+        :rtype: None
+        """
+        pass
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        pass
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        pass
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        pass
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+# @lc code=end
