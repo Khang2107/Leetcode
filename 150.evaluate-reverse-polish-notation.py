f3bec01399eab07260cb10b36abb94af82ba237b
nguvy056 <nguvy056@mymail.unisa.edu.au>
2023-03-21 23:39:59 +1030

    implement problem 150-evaluate-reverse

--- 150.evaluate-reverse-polish-notation.py
+++ 150.evaluate-reverse-polish-notation.py
@@ -11,6 +11,23 @@ class Solution(object):
         :type tokens: List[str]
         :rtype: int
         """
-        
-# @lc code=end
+        stack = []
 
+        for i in tokens:
+            if i not in "+-*/":
+                stack.append(int(i))
+            else:
+                right = stack.pop()
+                left = stack.pop()
+                if i == "+":
+                    stack.append(left + right)
+                elif i == "*":
+                    stack.append(left * right)
+                elif i == "-":
+                    stack.append(left - right)
+                elif i == "/":
+                    stack.append(int(float(left) / right))
+
+        return stack.pop()
+
+# @lc code=end
