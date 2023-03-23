7dec2176c2f665d3fcf6af9d0bc2e22046fb7188
nguvy056 <nguvy056@mymail.unisa.edu.au>
2023-03-23 17:36:35 +1030

    implement sol 2 problem 347-top-k-frequent-element

--- Array-Hashing/top-k-frequent-element-347.py
+++ Array-Hashing/top-k-frequent-element-347.py
@@ -63,3 +63,86 @@ class Solution(object):
             freq, item = heappop(heap)
             res.append(item)
         return res
+
+
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        # create a dictionary to store the frequency of each number in the list
+        freq_dict = {}
+        for num in nums:
+            # reverse the dictionary
+            # using priority queue
+            # most appeared elements will have smallest negative frequency in heap
+            if num not in freq_dict:
+                freq_dict[num] = 1
+            else:
+                # the more appeared the number, the smaller negative value is
+                # opposite to += 1
+                freq_dict[num] += 1
+        # print(freq_dict)
+
+        # create the heap from dictionary to sort the numbers based on their frequency
+        heap = []
+        # loop over frequency dict
+        for key in freq_dict:
+            # for each key, we have tuple (freq_dict[key], key)
+            # where freq_dict[key] is negative frequency of element represented by key
+            # after we push the tuple into heap, heap will sort in order based on the negative frequency:
+            # -3 first -> -1
+            # eg: {1:-3, 2:-2, 3:-1} -> (-3,1) (-2,2) (-1,3)
+            # eg: {1:-2, 2:-3, 3:-1} -> (-3,2) (-2,1) (-1,3)
+            # then we will push the tuple into heap
+            heappush(heap, (-freq_dict[key], key))
+            # print(heap)
+
+        res = []
+        for i in range(k):
+            # use heappop to remove element with smallest negative frequency from heap
+            # when heappop runs, it returns the tuple with the smallest frequency
+            # in form (frequency: negative, element)
+            freq, item = heappop(heap)
+            res.append(item)
+        return res
+
+
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        # create a dictionary to store the frequency of each number in the list
+        freq_dict = {}
+        for num in nums:
+            if num not in freq_dict:
+                freq_dict[num] = 1
+            else:
+                freq_dict[num] += 1
+
+        # create the heap from dictionary to sort the numbers based on their frequency
+        heap = []
+        # loop over frequency dict
+        for key in freq_dict:
+            # for each key, we have tuple (freq_dict[key], key)
+            # then we will push the tuple into heap
+            heappush(heap, (freq_dict[key], key))
+            # eg: {1:3, 2:2, 3:1} -> (1,3) (3,1) (2,2)
+            # pop the item not in range
+            # eg: {1:3, 2:2, 3:1} -> (3,1) (2,2) 
+            if k < len(heap):
+                heappop(heap)
+
+        res = []
+        while heap:
+            # use heappop to remove element with smallest negative frequency from heap
+            # when heappop runs, it returns the tuple with the smallest frequency
+            # in form (frequency: negative, element)
+            freq, item = heappop(heap)
+            res.append(item)
+        return res
